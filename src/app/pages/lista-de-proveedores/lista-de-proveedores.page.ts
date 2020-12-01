ab28619984f0f5c4d422cbf6b4a9b460d306ae5f
lquispe <lib.bioq@gmail.com>
2020-12-01 09:13:32 -0500

    Agregando lista de proveedores y su modal

---
+++ src/app/pages/lista-de-proveedores/lista-de-proveedores.page.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ProveedorInterface } from 'src/app/interfaces/proveedor';
+import { AgregarEditarProveedorPage } from 'src/app/modals/agregar-editar-proveedor/agregar-editar-proveedor.page';
+import { ProveedorRegistroService } from 'src/app/services/proveedor-registro.service';
+
+@Component({
+  selector: 'app-lista-de-proveedores',
+  templateUrl: './lista-de-proveedores.page.html',
+  styleUrls: ['./lista-de-proveedores.page.scss'],
+})
+export class ListaDeProveedoresPage implements OnInit {
+
+
+
+  listaDeProveedores: ProveedorInterface[];
+  proveedorItem: ProveedorInterface;
+
+  modalEvento: string;
+  modalTitle: String;
+  modalTag: string;
+  modalDataProveedor: ProveedorInterface;
+
+
+  constructor(private dataApi: ProveedorRegistroService, private modalCtlr: ModalController) {
+    //this.proveedoresForm = this.createFormGroupProveedor();
+    this.ObtenerProveedores();
+  }
+
+  ngOnInit() {
+  }
+
+
+  ObtenerProveedores(){
+    // console.log("getProveedores");
+
+    this.dataApi.ObtenerListaDeProveedores().subscribe(data => {
+      // console.log(data);
+      this.listaDeProveedores = data;
+      //console.log(this.proveedoressList.length);
+    });
+
+  }
+
+  AgregarNuevoProveedor(){
+    this.modalEvento = 'guardarProveedor';
+    this.modalTitle = 'Registrar nuevo proveedor';
+    this.modalTag = 'Guardar';
+    this.abrirModal();
+  }
+
+  ActualizarDataProveedor(proveedor: ProveedorInterface){
+
+    // console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+    // console.log(proveedor);
+    // console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
+    this.modalEvento = 'actualizarProveedor';
+    this.modalTitle = 'Actualizar datos del proveedor';
+    this.modalTag = 'Actualizar';
+    this.modalDataProveedor = proveedor;
+
+    setTimeout(() => {
+      this.abrirModal();
+    }, 500);
+
+  }
+
+
+  async abrirModal(){
+
+    const modal =  await this.modalCtlr.create({
+      component: AgregarEditarProveedorPage,
+      componentProps: {
+        eventoInvoker: this.modalEvento,
+        titleInvoker: this.modalTitle,
+        tagInvoker: this.modalTag,
+        dataInvoker: this.modalDataProveedor
+      }
+    });
+
+    await modal.present()
+  }
+
+
+
+
+}
