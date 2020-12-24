e60a8b4d81efb897fb90f001570ab49c2d130832
lquispe <lib.bioq@gmail.com>
2020-12-24 15:47:05 -0500

    Haciendo merge de ultimo momento

--- src/app/models/ProductoInterface.ts
+++ src/app/models/ProductoInterface.ts
@@ -2,13 +2,8 @@ export interface ProductoInterface extends ProgressEvent{
     id?: string;
     img?: string;
     nombre?: string;
-<<<<<<< HEAD
-    cantidad?: number;
-    //unidad?: string;
-=======
     cantidad?: number; // ?NOTE , numero de unidades en una venta
     unidad?: string; // !quitar
->>>>>>> 013d1bfcad6f9806b37fb6a2a01eca7b378fff5c
     precio?: number;
     sede?: string;
     medida?: string;    // !quitar
