98a905db7c2cbaadcf46d2500019da739b4189f8
AdrianLP <adrianllalli@gmail.com>
2020-12-27 18:56:50 -0500

    prointerface

--- src/app/models/ProductoInterface.ts
+++ src/app/models/ProductoInterface.ts
@@ -2,13 +2,8 @@ export interface ProductoInterface {
     id?: string;
     img?: string;
     nombre?: string;
-<<<<<<< HEAD
-    cantidad?: number;
-    //unidad?: string;
-=======
     cantidad?: number; // ?NOTE , numero de unidades en una venta
     unidad?: string; // !quitar
->>>>>>> 93223f58dd4a77a00491f25284e8c840f524d0c9
     precio?: number;
     sede?: string;
     medida?: string;    // !quitar
