cfd0421daaf003fd009e2e20c4305ab4216de6b9
AdrianLP <adrianllalli@gmail.com>
2020-12-29 18:07:05 -0500

    interface de agregar producto

--- src/app/modals/editar-producto/editar-producto.page.ts
+++ src/app/modals/editar-producto/editar-producto.page.ts
@@ -16,6 +16,12 @@ import { isNullOrUndefined } from 'util';
 export class EditarProductoPage implements OnInit {
   @Input()dataProducto;
 
+  // ----------------
+  processing:boolean;
+  uploadImage: string | ArrayBuffer;
+
+// ----------------
+  sinFoto: string;
   updateForm: FormGroup;
   image: any;
   mensaje: string;
@@ -39,6 +45,113 @@ export class EditarProductoPage implements OnInit {
     this.updateForm = this.createFormGroup();
     console.log('holaola',this.dataProducto);
   }
+
+    // --------------------------
+    presentActionSheet(fileLoader) {
+      fileLoader.click();
+      var that = this;
+      fileLoader.onchange = function () {
+        var file = fileLoader.files[0];
+        var reader = new FileReader();
+  
+        reader.addEventListener("load", function () {
+          that.processing = true;
+          that.uploadImage = reader.result;
+  
+          that.getOrientation(fileLoader.files[0], function (orientation) {
+            if (orientation > 1) {
+              that.resetOrientation(reader.result, orientation, function (resetBase64Image) {
+                that.uploadImage = resetBase64Image;
+              });
+            } else {
+              that.uploadImage = reader.result;
+            }
+          });
+        }, false);
+  
+        if (file) {
+          reader.readAsDataURL(file);
+        }
+      };
+    }
+  imageLoaded(){
+    this.processing = false;
+  }
+  
+  
+  getOrientation(file, callback) {
+    var reader = new FileReader();
+    reader.onload = function (e:any) {
+  
+      var view = new DataView(e.target.result);
+      if (view.getUint16(0, false) != 0xFFD8) return callback(-2);
+      var length = view.byteLength, offset = 2;
+      while (offset < length) {
+        var marker = view.getUint16(offset, false);
+        offset += 2;
+        if (marker == 0xFFE1) {
+          if (view.getUint32(offset += 2, false) != 0x45786966) return callback(-1);
+          var little = view.getUint16(offset += 6, false) == 0x4949;
+          offset += view.getUint32(offset + 4, little);
+          var tags = view.getUint16(offset, little);
+          offset += 2;
+          for (var i = 0; i < tags; i++)
+            if (view.getUint16(offset + (i * 12), little) == 0x0112)
+              return callback(view.getUint16(offset + (i * 12) + 8, little));
+        }
+        else if ((marker & 0xFF00) != 0xFF00) break;
+        else offset += view.getUint16(offset, false);
+      }
+      return callback(-1);
+    };
+    reader.readAsArrayBuffer(file);
+    console.log("fotobase100", this.uploadImage);
+  
+    this.image = this.uploadImage;
+  }
+  resetOrientation(srcBase64, srcOrientation, callback) {
+    var img = new Image();
+
+    img.onload = function () {
+      var width = img.width,
+        height = img.height,
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext("2d");
+
+      // set proper canvas dimensions before transform & export
+      if (4 < srcOrientation && srcOrientation < 9) {
+        canvas.width = height;
+        canvas.height = width;
+      } else {
+        canvas.width = width;
+        canvas.height = height;
+      }
+
+      // transform context before drawing image
+      switch (srcOrientation) {
+        case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+        case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+        case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+        case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+        case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+        case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+        case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+        default: break;
+      }
+
+      // draw image
+      ctx.drawImage(img, 0, 0);
+
+      // export base64
+      callback(canvas.toDataURL());
+    };
+
+    img.src = srcBase64;
+  }
+    removePic() {
+      this.uploadImage   = '../../../assets/fondoImg.jpg';
+    }
+    // -------------------------
 
   createFormGroup() {
     return new FormGroup({
@@ -82,7 +195,7 @@ export class EditarProductoPage implements OnInit {
       this.updateForm.value.nombre = this.updateForm.value.nombre.toLowerCase();
       if (this.image) {
         this.presentLoading();
-        this.uploadImage(this.image).then (url => {
+        this.uploadImages(this.image).then (url => {
           this.updateForm.value.img = url;
           console.log(this.updateForm.value);
           this.modalCtrl.dismiss({
@@ -113,7 +226,7 @@ export class EditarProductoPage implements OnInit {
     }
   }
 
-  uploadImage(image) {
+  uploadImages(image) {
     return new Promise<any>((resolve, reject) => {
       // tslint:disable-next-line:prefer-const
       let storageRef = this.firebaseStorage.storage.ref();
