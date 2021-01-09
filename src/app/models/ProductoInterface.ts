ff006b0a9c528b734202521479bd6ee409b067a7
AdrianLP <adrianllalli@gmail.com>
2021-01-09 15:00:50 -0500

    agregar codbarra

--- src/app/models/ProductoInterface.ts
+++ src/app/models/ProductoInterface.ts
@@ -13,6 +13,7 @@ export interface ProductoInterface {
     subCategoria?: string;
     descripcionProducto?: string;
     marca?: string;
+    codigoBarra?: string;
     fechaTimeRegistro?: Date;
     fechaDeVencimiento?: Date;
 
