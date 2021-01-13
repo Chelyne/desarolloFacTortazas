03293ca1202bc927efe3afa427760b7d0fe2433b
nerioch <hurcayanerio@gmail.com>
2021-01-13 17:53:28 -0500

    Ventas rediseniado

--- src/app/pages/confirmar-venta/confirmar-venta.page.ts
+++ src/app/pages/confirmar-venta/confirmar-venta.page.ts
@@ -292,7 +292,7 @@ export class ConfirmarVentaPage implements OnInit {
     this.venta.totalPagarVenta = this.importeTotal;
     this.venta.igv = this.igvImporteBase;
     this.venta.montoBase = this.importeBase;
-    this.venta.estadoVenta = 'conforme';
+    this.venta.estadoVenta = 'registrado';
     this.venta.cantidadBolsa = this.cantidadBolsa;
     this.venta.montoPagado = this.montoEntrante;
     console.log('Se generó el pago');
